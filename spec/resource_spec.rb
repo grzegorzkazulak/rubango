8048d90f4c2d089a1ddaf520a9ef75ce14b3a5c7
Stanislav Kolotinskiy <stanislav.colotinschi@gmail.com>
2012-06-07 20:10:24 +0300

    Add spec to test activity assignment in sp_run method

--- spec/resource_spec.rb
+++ spec/resource_spec.rb
@@ -14,7 +14,7 @@ class ResourceClient
     true
   end
 
-  def self.action_name
+  def action_name
     :a
   end
 
@@ -23,8 +23,8 @@ end
 
 describe Totango::Resource do
   context 'when tracking multiple activities for a single action' do
-    let(:proc1) { proc { 1 == 2 } }
-    let(:proc2) { proc { 1 == 1 } }
+    let(:proc1) { proc { 1 == 2 } } # proc that returns false
+    let(:proc2) { proc { 1 == 1 } } # proc that returns true
 
     subject { ResourceClient.sp_trackers.detect { |t| t.action.to_s == "a" } }
 
@@ -36,6 +36,16 @@ describe Totango::Resource do
     describe 'tracker' do
       it 'should have a conditions hash' do
         subject.opts[:conditions].keys.should =~ ["Activity1", "Activity2"]
+      end
+    end
+
+    describe '#sp_run' do
+      let(:rc) { ResourceClient.new }
+
+      it 'should set the activity corresponding to proc that returns true' do
+        Totango.should_receive(:track) { true }
+        rc.send(:sp_run)
+        subject.opts[:activity].should == "Activity2"
       end
     end
   end
